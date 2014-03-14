f0efd636de75afd6c056d2d2cecde275213803ad
asikowitz <nysiko@gmail.com>
2014-03-13 20:54:05 -0400

    No added arrays in Quicksort now, only temp vars

--- 07-Quicksort/Quicksort.java
+++ 07-Quicksort/Quicksort.java
@@ -7,30 +7,40 @@ public class Quicksort {
     public int[] qsort(int[] a, int low, int high) { //inclusive
 	if (high-low <= 0)
 	    return a;
-	    
-	//Copied from homework 06
 
-	int[] b = new int[a.length];
-	int h = high, l = low;
+	int store;
+	int wall = low;
 	int pivot = r.nextInt(high-low)+low; //pick pivot
-	for (int i=low; i<=high; i++) { //partitioning
-	    if (a[i] > a[pivot]) {
-		b[h] = a[i];
-		h--;
-	    }
-	    else if (a[i] < a[pivot]){
-		b[l] = a[i];
-		l++;
-	    }
-	}
-	while (l <= h) { //deals with duplicates
-	    b[l] = a[pivot];
-	    l++;
-	}
-	pivot = h;
+	boolean valid = true;
+	int c;
 
-	for (int i=low; i<=high; i++) //Copy back
-	    a[i] = b[i];
+	store = a[pivot]; //Moves pivot to last place
+	a[pivot] = a[high];
+	a[high] = store;
+	pivot = high;
+
+	for (int i=low; i<high && valid; i++) {
+	    c = wall+1;
+	    if (i != pivot) {
+		if (a[i] <= a[pivot])
+		    wall++;
+		else {
+		    while (c < high && a[c] > a[pivot])
+			c++;
+		    if (c < high) {
+			store = a[c];
+			a[c] = a[i];
+			a[i] = store;
+			wall++;
+		    }
+		}
+	    }
+	}
+
+	store = a[pivot]; //Moves pivot to wall
+	a[pivot] = a[wall];
+	a[wall] = store;
+	pivot = wall;
 
 	int[] R1 = qsort(a, low, pivot-1);
 	int[] R2 = qsort(a, pivot+1, high);
@@ -53,6 +63,7 @@ public class Quicksort {
 	    a[i] = r.nextInt(100);
 
 	Quicksort q = new Quicksort();
+	System.out.println(Arrays.toString(a));
 	System.out.println(Arrays.toString(q.qsort(a,0,n-1)));
     }
 }
