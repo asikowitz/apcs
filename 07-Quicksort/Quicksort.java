4e05b195f0f8edbb67cbe727e8db1a800d6e0c93
asikowitz <nysiko@gmail.com>
2014-03-17 00:06:27 -0400

    Quicksort 07b

--- 07-Quicksort/Quicksort.java
+++ 07-Quicksort/Quicksort.java
@@ -11,45 +11,56 @@ public class Quicksort {
 	int store;
 	int wall = low;
 	int pivot = r.nextInt(high-low)+low; //pick pivot
-	boolean valid = true;
-	int c;
+	int c = wall+1;
 
 	store = a[pivot]; //Moves pivot to last place
 	a[pivot] = a[high];
 	a[high] = store;
 	pivot = high;
 
-	for (int i=low; i<high && valid; i++) {
-	    c = wall+1;
-	    if (i != pivot) {
-		if (a[i] <= a[pivot])
+	for (int i=low; i<high; i++) {
+	    if (a[i] < a[pivot]) {
+		c++;
+		wall++;
+	    }
+	    else {
+		while (c < high && a[c] >= a[pivot])
+		    c++;
+		if (c < high) {
+		    store = a[c];
+		    a[c] = a[i];
+		    a[i] = store;
 		    wall++;
-		else {
-		    while (c < high && a[c] > a[pivot])
-			c++;
-		    if (c < high) {
-			store = a[c];
-			a[c] = a[i];
-			a[i] = store;
-			wall++;
-		    }
 		}
 	    }
 	}
 
-	store = a[pivot]; //Moves pivot to wall
-	a[pivot] = a[wall];
-	a[wall] = store;
-	pivot = wall;
+	int rwall = wall;
+	
+	for (int j=wall; j < high; j++) {
+	    if (a[j] == a[pivot]) {
+		store = a[j];
+		a[j] = a[rwall];
+		a[rwall] = store;
+		rwall++;
+	    }
+	}
 
-	int[] R1 = qsort(a, low, pivot-1);
-	int[] R2 = qsort(a, pivot+1, high);
+	System.out.println((rwall + wall)/2); //Not sure what the purpose of this is
+
+	store = a[pivot]; //Moves pivot to rwall
+	a[pivot] = a[rwall];
+	a[rwall] = store;
+
+	int[] R1 = qsort(a, low, wall-1);
+	int[] R2 = qsort(a, rwall+1, high);
 	int[] ans = new int[a.length];
 
-	for (int i=low; i<=pivot-1; i++)
+	for (int i=low; i<wall; i++)
 	    ans[i]=R1[i];
-	ans[pivot] = a[pivot];
-	for (int i=pivot+1; i<=high; i++)
+	for (int i=wall; i<=rwall; i++)
+	    ans[i] = a[wall];
+	for (int i=rwall+1; i<=high; i++)
 	    ans[i]=R2[i];
 
 	return ans;
@@ -60,7 +71,7 @@ public class Quicksort {
 	final int n = 100;
 	int[] a = new int[n];
 	for (int i=0; i<n; i++)
-	    a[i] = r.nextInt(100);
+	    a[i] = r.nextInt(15);
 
 	Quicksort q = new Quicksort();
 	System.out.println(Arrays.toString(a));
